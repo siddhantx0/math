5b34c4e11dfbdbef4ee6a3f759325bbb25a534e2
Siddhant Singh <singhsiddhantakshat@gmail.com>
2024-06-05 18:10:31 -0500

    on MOMs.

--- Main.py
+++ Main.py
@@ -1,6 +1,15 @@
+from flask import Flask, request
+# from flask_socketio import SocketIO, emit, join_room
 import time
 
-while True:
-    print(time.time())
+app = Flask(__name__)
+app.secret_key = time.time()
+# socketio = SocketIO(app, cors_allowed_origins="*")
+
+# while True:
+#     print(time.time())
+
+# if __name__ == '__main__':
+# socketio.run(app, host="0.0.0.0", port=9000)
 
 # https://x.com/panopstor/status/1798487147915149347
