c178e3158c859800d5509972ffc3a243d6960d57
Siddhant Singh <singhsiddhantakshat@gmail.com>
2024-06-07 12:57:42 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -10,13 +10,20 @@ while (app.secret_key):
 while True:
     print("hello world")
     print(time.time())
-    enc = tiktoken.get_encoding("o200k_base")
-    assert enc.decode(enc.encode("hello world")) == "hello world"
-    # To get the tokeniser corresponding to a specific model in the OpenAI API:
-    enc = tiktoken.encoding_for_model("gpt-4o")
+
+# while True:
+#     enc = tiktoken.get_encoding("o200k_base")
+#     assert enc.decode(enc.encode("hello world")) == "hello world"
+#     # To get the tokeniser corresponding to a specific model in the OpenAI API:
+#     enc = tiktoken.encoding_for_model("gpt-4o")
 
 
 if __name__ == '__main__':
     socketio.run(app, host="0.0.0.0", port=9000)
 
 # https://x.com/panopstor/status/1798487147915149347
+# correct context
+# persistence
+# colloquial
+# makes sense?
+# ALL KV.
